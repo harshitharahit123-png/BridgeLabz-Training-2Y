67689f4c0db0b78074698b9fba163bd7431de1d2
harshitharahit123-png <harshitharahit123@gmail.com>
2025-09-03 10:21:27 +0530

    Update and rename multiplication_array.java to mult_array.java

--- array/Level1/mult_array.java
+++ array/Level1/mult_array.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class multiplication_array{
+public class mult_array{
     public static void main(String[] args){
         Scanner scanner =  new Scanner(System.in);
         System.out.print("Enter the number for multiplication table: ");
@@ -12,4 +12,5 @@ public class multiplication_array{
             System.out.println(number + " x " + (i+1) + " = " + arr[i]);
         }
     }
+
 }
